2c7580c168d48e67bfc1a5e2b17b58f0a477b9e7
neojarvis <techadmin@iamneo.ai>
2025-09-18 15:16:08 +0000

    Auto-save-commit-9/18/2025--8:46:06-PM

--- springapp/src/main/java/com/examly/springapp/service/PetService.java
+++ springapp/src/main/java/com/examly/springapp/service/PetService.java
@@ -1,6 +1,13 @@
 package com.examly.springapp.service;
 
+import com.examly.springapp.model.Pet;
+import com.examly.springapp.repository.PetRepository;
+
 public class PetService {
     
+    PetRepository petRepo;
+    public Pet createPet(Pet pet){
+        return petRepo.save(pet);
+    }
 
 }

14a887b7dedb623d462bcd54014d96ca4bb6216b
neojarvis <techadmin@iamneo.ai>
2025-09-18 16:46:16 +0000

    Auto-save-commit-9/18/2025--10:16:14-PM

--- springapp/src/main/java/com/examly/springapp/controller/PetController.java
+++ springapp/src/main/java/com/examly/springapp/controller/PetController.java
@@ -1,5 +1,7 @@
 package com.examly.springapp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.examly.springapp.model.Pet;
@@ -7,18 +9,25 @@ import com.examly.springapp.service.PetService;
 
 
 @RestController
-@RequestMapping("/api/pets")
 public class PetController {
     
 @Autowired
-PetService petsrev;
+PetService petService;
 
-@PostMapping
+@PostMapping("/api/pets")
 public Pet create (@RequestBody Pet pet){
-return petsrev.createPet(pet);
+return petService.createPet(pet);
 }
 
-@GetMapping
-public 
+@GetMapping("/api/pets")
+public List<Pet> getAllPets(){
+    
+    return petService.getAllPets();
+}
 
+
+@PutMapping("/api/pets/{id}")
+public String updatePet(@PathVariable  Long id,@RequestBody Pet pet){
+    return petService.updatePet(id, pet);
+    }
 }
--- springapp/src/main/java/com/examly/springapp/service/PetService.java
+++ springapp/src/main/java/com/examly/springapp/service/PetService.java
@@ -1,5 +1,7 @@
 package com.examly.springapp.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,9 +13,24 @@ import com.examly.springapp.repository.PetRepository;
 public class PetService {
     @Autowired
     PetRepository petRepo;
-    
+
     public Pet createPet(Pet pet){
         return petRepo.save(pet);
     }
+
+public List<Pet> getAllPets(){
+    return petRepo.findAll();
+}
+
+public String updatePet(Long id,Pet pet){
+    if(petRepo.existsById(id)){
+        petRepo.save(pet);
+        return "updated successfully";
+    }
+    else{
+        return "id not Found"
+      }
+    }
+
 
 }

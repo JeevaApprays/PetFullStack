3ffad2479e1209005548956823d10188d889a1d7
neojarvis <techadmin@iamneo.ai>
2025-09-19 09:49:47 +0000

    Auto-save-commit-9/19/2025--3:19:44-PM

--- springapp/src/main/java/com/examly/springapp/controller/PetController.java
+++ springapp/src/main/java/com/examly/springapp/controller/PetController.java
@@ -2,13 +2,11 @@ package com.examly.springapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.examly.springapp.model.Pet;
 import com.examly.springapp.service.PetService;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-
 
 
 @RestController

2a98ee70e7cc33325e6bbbdd7642dbaa8065e5ac
neojarvis <techadmin@iamneo.ai>
2025-09-19 06:49:38 +0000

    Auto-save-commit-9/19/2025--12:19:35-PM

--- springapp/src/main/java/com/examly/springapp/model/Pet.java
+++ springapp/src/main/java/com/examly/springapp/model/Pet.java
@@ -1,10 +1,23 @@
 package com.examly.springapp.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Pet {
-    
 
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private Long id;
+    private String name;
+    private String species;
+    private String breed;
+    private int age;
+    private String description;
+    private String imageUrl;
+    private String adoptionSstatus;
+
 }

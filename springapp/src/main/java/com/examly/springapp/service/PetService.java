88c9285f155a9b47fcb127abc272b396f2625ebb
neojarvis <techadmin@iamneo.ai>
2025-09-22 03:46:25 +0000

    Auto-save-commit-9/22/2025--9:16:24-AM

--- springapp/src/main/java/com/examly/springapp/service/PetService.java
+++ springapp/src/main/java/com/examly/springapp/service/PetService.java
@@ -20,33 +20,5 @@ public Pet createPet(Pet pet){
     return petrepo.save(pet);
 }
 
-public List<Pet> getAllPets(){
-    return petrepo.findAll(); }
-
-public Optional<Pet> getPetById(Long id){ 
-   return petrepo.findById(id); }
-
-
-
-   public Optional<Pet> updatePet(Long id, Pet updatedPet) {
-    return petrepo.findById(id).map(existingPet -> {
-        // set fields (update only what you need)
-        existingPet.setName(updatedPet.getName());
-        existingPet.setAge(updatedPet.getAge());
-        existingPet.setAdoptionSstatus(updatedPet.getAdoptionSstatus());
-        existingPet.setDescription(updatedPet.getDescription());
-        
-        return petrepo.save(existingPet);
-    });
-}
-
-// ✅ Delete Pet
-public boolean deletePet(Long id) {
-    return petrepo.findById(id).map(pet -> {
-        petrepo.delete(pet);
-        return true;
-    }).orElse(false);
-}
-
 
 }

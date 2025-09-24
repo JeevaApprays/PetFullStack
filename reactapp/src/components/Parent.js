8b796b6e55cee86a15cecfcbe38ef76f990cbedb
neojarvis <techadmin@iamneo.ai>
2025-09-24 16:44:00 +0000

    Auto-save-commit-9/24/2025--10:14:00-PM

--- reactapp/src/components/Parent.js
+++ reactapp/src/components/Parent.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Child from './Child'
 
 const Parent = () => {
+    const[a,seta]=useState(1);
+    const add =()=>{
+      seta(a+1);
+    }
+    const sub =()=>{
+        seta(a-1);
+    }
+    useEffect (()=>{
+        console.log("1");
+    });
+    useEffect (()=>{
+        console.log("2");
+    },[]);
+    useEffect (()=>{
+        console.log("3");
+    },[a]);
+
+
   return (
     <div>
         <Child message = "Here is my props demo"></Child>
+        <h1>{a}</h1>
+        <button onClick={add}>Inc</button>
+        <button onClick={sub}>Dec</button>
     </div>
   )
 }
-
 export default Parent

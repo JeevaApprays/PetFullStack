4c67c188dd991da4ee2e338d8ad8bfd2ebbec0b2
neojarvis <techadmin@iamneo.ai>
2025-09-24 16:29:00 +0000

    Auto-save-commit-9/24/2025--9:59:00-PM

---
+++ reactapp/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Demo from './components/Demo'
+import Parent from './components/Parent'
+
+const App = () => {
+  return (
+    <div>
+        <Parent></Parent>
+    </div>
+  )
+}
+
+export default App

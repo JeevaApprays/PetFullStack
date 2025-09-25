2a3ada2dee76be19d32b923b6474fb615515f8ff
neojarvis <techadmin@iamneo.ai>
2025-09-25 08:31:48 +0000

    Auto-save-commit-9/25/2025--2:01:43-PM

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react'
 
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Demo from './components/Demo'
+import Header from './components/Header'
+import Login from './components/Login'
+
 
 const App = () => {
   return (
      <div>
-
-<Demo/>
-<sample/>
-
+      <BrowserRouter>
+      <Header/>
+      <Routes>
+         <Route path='/' element={<Demo/>}/>
+         <Route path="/login" element={<Login/>}/>
+      </Routes>
+      <Footer/>
+      </BrowserRouter>
      </div>
   )
 }
--- reactapp/src/components/sample.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const sample = () => {
-  return (
-    <div>Demo</div>
-  )
-}
-
-export default sample

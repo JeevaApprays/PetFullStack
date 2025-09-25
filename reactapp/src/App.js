d407a24ff721255571b3acd14acd06b150c07e5e
neojarvis <techadmin@iamneo.ai>
2025-09-25 01:20:11 +0000

    Auto-save-commit-9/25/2025--6:50:10-AM

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import Demo from './components/Demo'
+import {BrowserRouter, Route,Routes} from 'react-router-dom'
 import Parent from './components/Parent'
+import Header from './components/Header'
 
 const App = () => {
   return (
-    <div>
+      <BrowserRouter>
+
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Parent/>}/>
 
 
+      </Routes>
       
-        <Parent></Parent>
-    </div>
+      </BrowserRouter>
   )
 }
 

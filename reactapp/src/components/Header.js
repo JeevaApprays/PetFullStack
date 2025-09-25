61162d738efb275ae7ed265ba464a9b778fbce14
neojarvis <techadmin@iamneo.ai>
2025-09-25 01:58:41 +0000

    Manual-save-commit-9/25/2025--7:28:40-AM

--- reactapp/src/components/Header.js
+++ reactapp/src/components/Header.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="header">
-      <div className="logo">MyWebsite</div>
-      <nav className="nav">
-        <a href="/">Home</a>
+    <header style={{background:"#000fff"}}>
+      <h1 style={{textAlign:"center", color:"white"}}>AIML</h1>
+      <nav style={{display:"flex", gap:"30px", color:"white"}}>
+        <a style={{color:"white"}} href="/">Home</a>
         <a href="/demo">About</a>
         <a href="#services">Services</a>
         <a href="#contact">Contact</a>

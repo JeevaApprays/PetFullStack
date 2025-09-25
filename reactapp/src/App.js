f7d7112cafe7c26bebc5100f508ee47befb39ef1
neojarvis <techadmin@iamneo.ai>
2025-09-25 04:45:13 +0000

    Auto-save-commit-9/25/2025--10:15:12-AM

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import Demo from './components/Demo'
-import {BrowserRouter, Route,Routes} from 'react-router-dom'
-import Parent from './components/Parent'
-import Header from './components/Header'
+
 
 const App = () => {
   return (
-     <div></div>
+     <div>
+<h1>welcome to React JS</h1>
+
+     </div>
   )
 }
 
